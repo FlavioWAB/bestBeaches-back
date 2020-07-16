5ac275c45625c2bf6b7eb19e654211534fe88168
FlavioWAB <59736888+FlavioWAB@users.noreply.github.com>
2020-07-15 23:07:49 -0300

    POST removal and CORS support

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,12 @@
 import * as functions from 'firebase-functions';
 import express from "express";
 import BeachesController from './Controllers/BeachesController';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
+
 app.get('/beaches', BeachesController.get);
-app.post('/beaches', BeachesController.post);
 
 exports.api = functions.https.onRequest(app);
